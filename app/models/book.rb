171e0d475d6a9fd6ddae9ebe133ba55e39e6d3bf
Kazuma Ebina <kazuma.ebina@quipper.com>
2020-05-17 05:37:35 +0000

    feat: Book model を validate

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+  VALID_URL_REGEX = /\Ahttps?:\/\/[^\s\/$.?#].[^\s]*\z/
+
+  validates :name     , presence: true,    length: { maximum: 1000  }
+  validates :thumbnail, allow_blank: true, length: { maximum: 1000  }, format: { with: VALID_URL_REGEX }
+  validates :author   , presence: true,    length: { maximum: 1000  }
+  validates :url      , presence: true,    length: { maximum: 1000  }, format: { with: VALID_URL_REGEX }
+  validates :note     , allow_blank: true, length: { maximum: 10000 }
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @book = Book.new(
+      name: "My Book",
+      thumbnail: "https://example.com/image",
+      author: "Me",
+      url: "https://example.com/url",
+      is_read: false,
+      note: "My note",
+    )
+  end
+
+  test "valid" do
+    assert @book.valid?
+  end
+
+  test "required: name" do
+    @book.name = ""
+    assert_not @book.valid?
+  end
+
+  test "required: author" do
+    @book.author = ""
+    assert_not @book.valid?
+  end
+
+  test "required: url" do
+    @book.url = ""
+    assert_not @book.valid?
+  end
+
+  test "optional: thumbnail" do
+    @book.thumbnail = ""
+    assert @book.valid?
+  end
+
+  test "optional: note" do
+    @book.note = ""
+    assert @book.valid?
+  end
+
+  test "URL validation: thumbnail" do
+    @book.thumbnail = "ttp://"
+    assert_not @book.valid?
+  end
+
+  test "URL validation: url" do
+    @book.url = "ttp://"
+    assert_not @book.valid?
+  end
+
 end
