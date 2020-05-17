ac70b7ede8d3ec68be1eee871a9db40648a293c4
Kazuma Ebina <kazuma.ebina@quipper.com>
2020-05-17 07:44:53 +0000

    fix: camelCase <-> snake_case 変換をちゃんとした

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::BooksController < ApplicationController
 
   def index
-    books = Book.all.map do |b|
-      b.as_json.compact.deep_transform_keys { |k| k.camelize(:lower) }
-    end
+    books = Book.all.map { |b| as_json(b) }
 
-    render json: {
+    render status: :ok, json: {
       titles: books
     }
   end
@@ -14,11 +12,11 @@ class Api::V1::BooksController < ApplicationController
     book = Book.new(book_params)
 
     if book.save
-      render status: :created, json: book.as_json
+      render status: :created, json: as_json(book)
     else
       render status: :bad_request, json: {
         errors: book.errors.full_messages,
-        payload: book_params
+        payload: as_json(book)
       }
     end
   end
@@ -26,7 +24,11 @@ class Api::V1::BooksController < ApplicationController
   private
 
     def book_params
-      params.require(:book).permit(:name, :thumbnail, :author, :url, :note)
+      params.require(:book).permit(:name, :thumbnail, :author, :url, :note, :is_read)
+    end
+
+    def as_json(model)
+      model.as_json.compact.deep_transform_keys { |k| k.camelize(:lower) }
     end
 
 end
