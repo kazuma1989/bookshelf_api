c375778e1123fecbece6bdae61cedccff9d3814e
Kazuma Ebina <kazuma.ebina@quipper.com>
2020-05-17 07:10:46 +0000

    feat: POST で book を作成できる

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::BooksController < ApplicationController
+
   def index
     books = Book.all.map do |b|
       b.as_json.compact.deep_transform_keys { |k| k.camelize(:lower) }
@@ -8,4 +9,24 @@ class Api::V1::BooksController < ApplicationController
       titles: books
     }
   end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render status: :created, json: book.as_json
+    else
+      render status: :bad_request, json: {
+        errors: book.errors.full_messages,
+        payload: book_params
+      }
+    end
+  end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:name, :thumbnail, :author, :url, :note)
+    end
+
 end
